1243c8a131ff69121b253659c676ed0ca7273374
FraCabCam <fcabrera4084@ceuandalucia.es>
2023-11-22 20:07:41 +0100

    Primera lloracion im yoring

---
+++ Tema1/src/tema1/Ej01.java
@@ -0,0 +1,22 @@
+package tema1;
+
+import java.util.Scanner;
+
+public class Ej01 {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Nombre del usuario: ");
+
+		String nombre = scanner.nextLine();
+
+		System.out.println("Ahora su apellido ");
+
+		String apellido = scanner.nextLine();
+
+		System.out.println("Su nombre completo es " + nombre + " " + apellido);
+
+		scanner.close();
+	}
+}
---
+++ Tema3/src/Ejercicios01/Ej01.java
@@ -0,0 +1,23 @@
+package Ejercicios01;
+
+public class Ej01 {
+	public static void main(String[] args) {
+
+		Integer num[] = new Integer[15];
+
+		Integer contador = 0;
+
+		for (int i = 1; i <= 30; i++) {
+			if (i % 2 == 0) {
+				num[contador] = i;
+				contador++;
+
+			}
+
+		}
+
+		for (int i = 0; i < num.length; i++) {
+			System.out.println(num[i] + "\n ");
+		}
+	}
+}
